3d599647bbd460076dd38ddc0170ed15a9dcbbad
Chidori <business.faissal@outlook.com>
2022-04-03 00:40:14 -0500

    JWT implemented

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
 import { ADD_USER } from '../utils/mutations';
-
+import Auth from '../utils/auth';
 const Signup = () => {
   const [formState, setFormState] = useState({ username: '', email: '', password: '' });
   const [addUser, { error }] = useMutation(ADD_USER);
@@ -25,7 +25,7 @@ const Signup = () => {
       const { data } = await addUser({
         variables: { ...formState },
       });
-      console.log(data);
+      Auth.login(data.addUser.token);
     } catch (e) {
       console.error(e);
     }
